06e6d08dd2ad99564bf7f6464130f2fe554aa3c1
Võ Sỹ Nam <s2loveandhate@gmail.com>
2020-06-19 14:23:54 +0700

    huyngu

---
+++ Demo/Assets/Script/Nextlevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Nextlevel : MonoBehaviour
+{
+    public void Load(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
